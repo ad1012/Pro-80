1ed12f47c7172e9c220c72158b249952f54866d7
ad1012 <69478216+ad1012@users.noreply.github.com>
2021-06-21 13:08:30 -0500

    Add files via upload

--- App.js
+++ App.js
@@ -2,11 +2,9 @@ import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
 import {createAppContainer,createSwitchNavigator} from 'react-navigation'
 import {createBottomTabNavigator} from 'react-navigation-tabs'
-import LoginScreen from './screens/LoginScreen';
-import ExchangeScreen from './screens/ExchangeScreen';
-import HomeScreen from './screens/HomeScreen';
-import CustomSideBar from './components/CustomSideBar';
-import SettingsScreen from './screens/SettingsScreen'
+import LoginScreen from './screens/LoginScreen'
+import AppDrawNavigator from './components/AppDrawerNavigator'
+import { AppTabNavigator } from './components/AppTabNavigator';
 
 export default function App() {
   return (
@@ -14,56 +12,10 @@ export default function App() {
   );
 }
 
-const TabNavigator = createBottomTabNavigator({
-  Home:{screen: HomeScreen},
-  Exchange:{screen: ExchangeScreen}
-},
-
-{
-defaultNavigationOptions: ({navigation})=>({
-  tabBarIcon: ()=>{
-    const routeName = navigation.state.routeName;
-    if(routeName === "HomeScreen"){
-      return(
-        <Image
-        source={require("./assets/Home.png")}
-        style={{width:20, height:20}}
-      />
-      )
-
-    }
-    else if(routeName === "Exchange"){
-      return(
-        <Image
-        source={require("./assets/exchange.png")}
-        style={{width:20, height:20,}}
-      />)
-
-    }
-  }
-})
-}
-);
-
-const AppDrawNavigator = createDrawerNavigator({ 
-  Home : { 
-    screen : TabNavigator 
-  }, 
-  Settings : { 
-    screen : SettingsScreen
-   } 
-  }, 
-  { 
-    contentComponent: CustomSideBar 
-  }, 
-  { 
-    initialRouteName : 'Home' 
-  })
-
   const switchNavigator = createSwitchNavigator({ 
-    WelcomeScreen:{screen: LoginScreen}, 
-    AppDrawNavigator : AppDrawNavigator,
+    LoginScreen:{screen: LoginScreen}, 
+    AppDrawerNavigator : AppDrawerNavigator,
+    AppTabNavigator : AppTabNavigator,
   })
 
 const AppContainer = createAppContainer(switchNavigator);
-
